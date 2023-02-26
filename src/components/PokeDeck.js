7cf89cd0f7293f773471c014e697c7f8a0607cc1
mathiaskerr <114953633+mathiaskerr@users.noreply.github.com>
2023-02-26 18:03:19 +0000

    colour'

--- src/components/PokeDeck.js
+++ src/components/PokeDeck.js
@@ -1,21 +1,21 @@
 const PokeDeck= ({favPokemon}) => {
 
   const favouriteNodes = favPokemon.map((pokemon,index) => {
-     
+     console.log(pokemon.types[0].type.name)
     return (
-     <ul> 
+     <ul className={pokemon.types[0].type.name}> 
      <li><h3>{pokemon.name}</h3></li>
-    <li key={index}><img id='fav-img'src={pokemon.sprites.other['official-artwork'].front_default}></img></li>
+    <li key={pokemon.types[0].type.name}><img id='fav-img'src={pokemon.sprites.other['official-artwork'].front_default}></img></li>
     </ul>
   )})
     return(
-      <div>
+      <div >
           <h2>Pokedex</h2>
           
           <ul className="fav">
             {favouriteNodes}
 
-            
+
           </ul>
       </div>
     )
